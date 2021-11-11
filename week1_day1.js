bad25e99425c328db6bd0c86b5de993acf3f11f3
elmehdi rhindi <g6handi@gmail.com>
2021-11-11 21:08:07 +0100

    week 1 day 1 and 2

---
+++ week1_day1.js
@@ -0,0 +1,39 @@
+// some comment
+
+/*
+multi
+line
+comments
+*/
+
+// ES5
+var myVariable = 5
+
+//ES6
+let var2 = 1
+const var3 = 2
+
+var2 = 3
+var3 = 4 // no
+
+let $variable1
+
+let MyNewVariable //pascal case
+let my_new_variable // kebab case
+let myNewVariable // camel case
+let $myNewVariable
+
+let myVariableNumber = 2
+myVariableNumber = 'foo bar'
+myVariableNumber = null
+
+let charInsideName
+
+// web
+let stuffPlaceholder = null
+
+stuffPlaceholder = 1 + 2
+
+// ...
+
+stuffPlaceholder = 'foo bar'
---
+++ week1_day2.js
@@ -0,0 +1,57 @@
+/* let a = 2
+
+if (a === 1) {
+    console.log('hurray!')
+} */
+
+/* let fruits = ['orange', 'banana']
+
+if (fruits[1] === 'banana') {
+    console.log('yes that is a banana')
+} else {
+    console.log('no that is not a banana')
+} */
+
+/* let fruits = ['orange', 'banana', 'avocado', 'kiwi', 'mango']
+let name = 'elmehdi'
+
+for (let i=4; i>=0; i=i-2) {
+    console.log(fruits[i])
+} */
+
+/* let fruits = ['orange', 'banana', 'avocado', 'kiwi', 'mango']
+
+switch (fruits[2]) {
+    case 'orange':
+        console.log('it is an orange')
+        break
+    case 'banana':
+        console.log('it is a banana')
+        break
+    default:
+        console.log('do not know')
+} */
+
+/* let i = 0
+
+while (i < 0) {
+    console.log(`hello ${i}`)
+    i = i + 1
+}
+
+let j = 0
+
+do {
+    console.log(`hello ${j}`)
+    j = j + 1
+} while (j < 0) */
+
+let fruits = ['orange', 'banana', 'avocado', 'kiwi', 'mango']
+
+for (let fruit of fruits) {
+    console.log(fruit)
+}
+
+for (let i=0; i<fruits.length; i++) {
+    console.log(fruits[i])
+}
