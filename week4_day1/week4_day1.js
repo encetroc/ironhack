85205611eaec2283371a6959de4ee1f5bcdef1b5
elmehdi rhindi <g6handi@gmail.com>
2021-11-30 19:15:58 +0100

    week 4 day 1 commit 1

---
+++ week4_day1/week4_day1.js
@@ -0,0 +1,23 @@
+const itemList = document.querySelectorAll(".itemList .item")
+
+const mydiv = document.querySelector('div > .item')
+
+const mylist = document.querySelector('.itemList')
+
+const myElement = document.getElementsByTagName('div')
+
+const div1 = document.querySelector('#div1')
+
+mylist.className = 'bgRed'
+
+const divId = div1.getAttribute('id')
+
+const myInput = document.querySelector('input')
+
+myInput.getAttribute('placeholder')
+
+myInput.setAttribute('placeholder', 'hello world')
+
+myInput.removeAttribute('placeholder')
+
+console.log(myInput.getAttribute('placeholder'))
