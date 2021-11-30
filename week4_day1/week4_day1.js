91f8a591e9ec147f485770fe7b6c468d5dfed449
elmehdi rhindi <g6handi@gmail.com>
2021-11-30 20:31:36 +0100

    week 4 day 1 commit 3

--- week4_day1/week4_day1.js
+++ week4_day1/week4_day1.js
@@ -1,4 +1,4 @@
-const itemList = document.querySelectorAll(".itemList .item")
+const items = document.querySelectorAll(".itemList .item")
 
 const mydiv = document.querySelector('div > .item')
 
@@ -8,14 +8,42 @@ const myBtn = document.querySelector('.addBtn')
 
 const myInput = document.querySelector('.input')
 
+const deleteBtns = document.querySelectorAll('.deleteBtn')
+
+deleteBtns.forEach(deleteBtn => {
+  deleteBtn.onclick = () => {
+    const parent = deleteBtn.parentElement
+    parent.remove()
+  }
+})
+
+items.forEach(item => {
+  item.onclick = () => {
+    item.classList.toggle("completedItem")
+  }
+})
+
 /* const newText = document.createTextNode('item 4')
 newItem.appendChild(newText) */
 
 const addListitem = (itemText) => {
+  // create list item
   const newItem = document.createElement('li')
   newItem.className = 'item'
-  newItem.innerHTML = itemText
+  // create text of the list item
+  const newText = document.createTextNode(`${itemText} `)
+  newItem.appendChild(newText)
+  // create the delete button
+  const newDeleteBetn = document.createElement('button')
+  newDeleteBetn.innerText = 'delete'
+  newItem.appendChild(newDeleteBetn)
+  // add the list item to our list
   mylist.appendChild(newItem)
+  // add the click listener to the delete button
+  newDeleteBetn.onclick = () => {
+    const parent = newDeleteBetn.parentElement
+    parent.remove()
+  }
 }
 
 myBtn.onclick = () => {
