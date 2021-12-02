519192c13245a202bd04797203813a66ea036311
elmehdi rhindi <g6handi@gmail.com>
2021-12-02 20:41:46 +0100

    week 4 day 2 commit 3

--- js-testing-jest/spec/sum.spec.js
+++ js-testing-jest/spec/sum.spec.js
@@ -1,8 +1,20 @@
-const sum = require("../src/sum")
+const sum = require("../src/sum");
 
 describe("sum tests", () => {
   it("should give back the correct sum", () => {
     const result = sum([1, 2, 3]);
     expect(result).toEqual(6);
+  });
+  it("should give back 0 for empty array", () => {
+    const result = sum([]);
+    expect(result).toEqual(0);
+  });
+  it("should not pass nothing", () => {
+    const fn = () => sum();
+    expect(fn).toThrow(new Error("you should pass an array to this funtion"));
+  });
+  it("should pass an array", () => {
+    const fn = () => sum(1);
+    expect(fn).toThrow(new Error("you should pass an array to this funtion"));
   });
 });
--- js-testing-jest/src/sum.js
+++ js-testing-jest/src/sum.js
@@ -1,8 +1,16 @@
 const sum = (array) => {
-  const total = array.reduce((accumulator, value) => {
-    return accumulator + value;
-  });
-  return total;
+  if (!array || !Array.isArray(array)) {
+    throw new Error("you should pass an array to this funtion");
+  }
+
+  if (array.length === 0) {
+    return 0;
+  } else {
+    const total = array.reduce((accumulator, value) => {
+      return accumulator + value;
+    });
+    return total;
+  }
 };
 
 module.exports = sum;
